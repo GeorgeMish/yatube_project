22f5d7d11569175560480e09f8c053b7b86d3397
Yury Mishkevich <georgemishkevich@gmail.com>
2022-10-28 14:26:43 +0300

    Спринт 3/11 → Тема 4/7: Шаблоны и теги Django → Урок 4/6 ост. тут

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Главная страница
     path('', views.index),
-    # Страница со списком групп
     path('group/', views.posts_groups_list),
-    # Страница с постами одной группы
-    path('group/<slug:slug>', views.group_posts),
+    path('group/<slug:slug>/', views.group_posts),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
-# Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
-# Страница со списком групп
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {'title': title,}
+    return render(request, template, context)
+
+
 def posts_groups_list(request):
-    return HttpResponse('Список групп')
-# Страница с конкретной группой
+    template = 'posts/posts_list'
+    return render(request, template)
+
+
 def group_posts(request, slug):
-    return HttpResponse(f'Посты группы {slug}')
+    #group = get_object_or_404(group, slug=slug)
+    template = 'posts/group_list.html'
+    title = '"Здесь будет информация о группах проекта Yatube"'
+    posts = Post.object.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts}
+    return render(request, template, context)
